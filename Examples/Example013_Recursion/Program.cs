2bf8374107a1aa61aac80c348f6657f939440d64
Vyacheslav Shirshov <vd.shirshov@gmail.com>
2022-07-11 00:41:20 +0300

    Двумерные массивы. Фибо.

--- Examples/Example013_Recursion/Program.cs
+++ Examples/Example013_Recursion/Program.cs
@@ -111,6 +111,7 @@ PrintImage(pic);
 
 */
 
+/*
 double Factorial(int n)
 {
     // 1! = 1
@@ -122,4 +123,23 @@ for (int i=1; i < 40; i++ )
 {
 Console.WriteLine($"{i}! = {Factorial(i)}"); 
 }
+*/
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+
+/*
+double Fibonacci(int n)
+{
+    if(n == 1 || n = 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i=1; i < 50; i++ )
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+*/
 
